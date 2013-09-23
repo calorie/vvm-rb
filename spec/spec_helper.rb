5219dd7c6e1a8c781f423e5bb595142e39ea7968
YuuShigetani <s2g4t1n2@gmail.com>
2013-09-23 23:54:12 +0900

    add switcher spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,37 +9,50 @@ require 'rspec'
 require 'fileutils'
 require 'tmpdir'
 require 'vvm-rb'
-require 'vvm-rb/constants'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  config.before :all do
+  config.before :suite do
     clear_consts
-    @cache = File.expand_path(File.join(File.dirname(__FILE__), '..', '.vvm_cache'))
-    DOT_DIR = @cache
-    unless Dir.exists?(@cache)
-      FileUtils.mkdir_p(@cache)
+    CACHE_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..', '.vvm_cache'))
+    DOT_DIR   = CACHE_DIR
+    ETC_DIR   = "#{DOT_DIR}/etc"
+    REPOS_DIR = "#{DOT_DIR}/repos"
+    SRC_DIR   = "#{DOT_DIR}/src"
+    VIMS_DIR  = "#{DOT_DIR}/vims"
+    VIM_URI   = 'https://vim.googlecode.com/hg/'
+    unless Dir.exists?(CACHE_DIR)
+      FileUtils.mkdir_p(CACHE_DIR)
       Cli.start('install v7-3-969'.split)
       Cli.start('install v7-4'.split)
     end
   end
 
-  config.before :each do
+  config.before :all do
     clear_consts
     @tmp = Dir.mktmpdir
-    FileUtils.cp_r(@cache, @tmp)
-    DOT_DIR = File.expand_path(File.join(@tmp, '.vvm_cache'))
-    p @tmp
+    FileUtils.cp_r(CACHE_DIR, @tmp)
+    DOT_DIR   = File.expand_path(File.join(@tmp, '.vvm_cache'))
+    ETC_DIR   = "#{DOT_DIR}/etc"
+    REPOS_DIR = "#{DOT_DIR}/repos"
+    SRC_DIR   = "#{DOT_DIR}/src"
+    VIMS_DIR  = "#{DOT_DIR}/vims"
   end
 
-  config.after :each do
-    # FileUtils.rm_rf(@tmp)
+  config.after :all do
+    FileUtils.rm_rf(@tmp)
   end
 end
 
 def clear_consts
-  %w{ DOT_DIR }.each do |c|
+  %w{
+    DOT_DIR
+    ETC_DIR
+    REPOS_DIR
+    SRC_DIR
+    VIMS_DIR
+  }.each do |c|
     const = c.to_sym
     Object.send(:remove_const, const) if Object.const_defined?(const)
   end
--- spec/switcher_spec.rb
+++ spec/switcher_spec.rb
@@ -2,7 +2,42 @@ require 'spec_helper'
 
 describe 'Switcher' do
   describe 'use' do
-    it 'can switch vim version' do
+    context 'system vim' do
+      before :all do
+        switcher = Switcher.new('system')
+        switcher.use
+      end
+
+      it 'delete current' do
+        expect(Dir.exists?("#{VIMS_DIR}/current")).not_to be_true
+      end
+    end
+
+    context 'different version' do
+      before :all do
+        switcher = Switcher.new('v7-4')
+        switcher.use
+        @vims_dir = "#{VIMS_DIR}/v7-4"
+        @current  = "#{VIMS_DIR}/current"
+      end
+
+      it 'exist current' do
+        expect(Dir.exists?(@current)).to be_true
+      end
+
+      it 'switch current' do
+        expect(File.readlink(@current)).to eq(@vims_dir)
+      end
+    end
+
+    context 'unknown version' do
+      before :all do
+        @switcher = Switcher.new('v7-5')
+      end
+
+      it 'raise error' do
+        expect(proc { @switcher.use }).to raise_error
+      end
     end
   end
 end
