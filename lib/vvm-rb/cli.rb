69f331382ca198d7d079b9835672509090a11b09
YuuShigetani <s2g4t1n2@gmail.com>
2013-12-08 15:20:23 +0900

    fix bug of latest installation & refactoring

--- lib/vvm-rb/cli.rb
+++ lib/vvm-rb/cli.rb
@@ -4,36 +4,38 @@ class Cli < Thor
   include Thor::Actions
   include VvmRb::Base
 
-  desc 'install [TAG] [options]', 'Install a specific version of Vim'
-  method_option :use, :type => :boolean, :aliases => '-u'
+  desc 'install [VERSION] [CONFIGURE_OPTS]', 'Install a specific version of Vim'
+  method_option :use, type: :boolean, aliases: '-u', banner: 'Use installed vim'
   def install(version, *conf)
-    installer(version, conf)
+    version = format_version(version)
+
+    Installer.fetch
+    i = Installer.new(version, conf)
+    i.checkout
+    i.configure
+    i.make_install
+    Installer.cp_etc
+
     use(version) if options[:use]
 
-    print "\e[32m"
-    puts <<-EOS
-
-Vim is successfully installed.  For daily use,
-please add the following line into your ~/.bash_login etc:
-
-test -f ~/.vvm-rb/etc/login && source ~/.vvm-rb/etc/login
-
-    EOS
-    print "\e[0m"
+    message
   end
 
-  desc 'reinstall [TAG] [options]', 'Reinstall a specific version of Vim'
+  desc 'reinstall [VERSION] [CONFIGURE_OPTS]', 'Reinstall a specific version'
   def reinstall(version, *conf)
-    Uninstaller.new(version).uninstall
-    installer(version, conf)
+    uninstall(version)
+    install(version, conf)
   end
 
-  desc 'rebuild [TAG] [options]', 'Rebuild a specific version of Vim'
+  desc 'rebuild [VERSION] [CONFIGURE_OPTS]', 'Rebuild a specific version of Vim'
   def rebuild(version, *conf)
-    rebuilder(version, conf)
+    r = Installer.new(version, conf)
+    r.make_clean
+    r.configure
+    r.make_install
   end
 
-  desc 'use [TAG]', 'Use a specific version of Vim as the default one.'
+  desc 'use [VERSION]', 'Use a specific version of Vim as the default one.'
   def use(version)
     Switcher.new(version).use
   end
@@ -49,7 +51,7 @@ test -f ~/.vvm-rb/etc/login && source ~/.vvm-rb/etc/login
     puts Version.versions.join("\n")
   end
 
-  desc 'uninstall [TAG]', 'Uninstall a specific version of Vim.'
+  desc 'uninstall [VERSION]', 'Uninstall a specific version of Vim.'
   def uninstall(version)
     Uninstaller.new(version).uninstall
   end
@@ -61,23 +63,24 @@ test -f ~/.vvm-rb/etc/login && source ~/.vvm-rb/etc/login
 
   private
 
-  def installer(version, conf)
-    Installer.fetch
+  def format_version(version)
     if version == 'latest'
       version = Version.list.select { |v| v =~ /^v7-.+$/ }.last
       new_version?(version)
     end
-    i = Installer.new(version, conf)
-    i.checkout
-    i.configure
-    i.make_install
-    Installer.cp_etc
+    return version
   end
 
-  def rebuilder(version, conf)
-    r = Installer.new(version, conf)
-    r.make_clean
-    r.configure
-    r.make_install
+  def message
+    print "\e[32m"
+    puts <<-EOS
+
+Vim is successfully installed.  For daily use,
+please add the following line into your ~/.bash_login etc:
+
+test -f ~/.vvm-rb/etc/login && source ~/.vvm-rb/etc/login
+
+    EOS
+    print "\e[0m"
   end
 end
