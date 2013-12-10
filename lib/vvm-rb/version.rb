e3ff3695c2239ce98b47558e36cf8163eb7efc33
YuuShigetani <s2g4t1n2@gmail.com>
2013-12-10 20:50:48 +0900

    fix bug, directory not found

--- lib/vvm-rb/version.rb
+++ lib/vvm-rb/version.rb
@@ -1,6 +1,8 @@
 class Version
   def self.list
-    Dir.chdir(get_vimorg_dir) do
+    vimorg_dir = get_vimorg_dir
+    return [] unless File.exists?(vimorg_dir)
+    Dir.chdir(vimorg_dir) do
       list = `hg tags`.split.reverse
       return list.values_at(* list.each_index.select { |i| i.odd? })
     end
@@ -8,7 +10,9 @@ class Version
 
   def self.versions
     output = []
-    Dir.glob(File.join(get_vims_dir, 'v*')).sort.each do |d|
+    vims_dir = get_vims_dir
+    return output unless File.exists?(vims_dir)
+    Dir.glob(File.join(vims_dir, 'v*')).sort.each do |d|
       output << File.basename(d)
     end
     return output
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,15 +1,36 @@
 require 'spec_helper'
+require 'fileutils'
 
 describe 'Version' do
   describe 'list' do
-    it 'echo available vim versions' do
-      expect(Version.list.join("\n")).to match(/start\n(v7-.+\n)+tip$/)
+    context 'vimorg dirctory exists' do
+      it 'echo available vim versions' do
+        expect(Version.list.join("\n")).to match(/start\n(v7-.+\n)+tip$/)
+      end
+    end
+    context 'vimorg dirctory is not found' do
+      before do
+        FileUtils.rm_rf(get_vimorg_dir)
+      end
+      it 'echo nothing' do
+        expect(Version.list).to eq []
+      end
     end
   end
 
   describe 'versions' do
-    it 'echo installed vim versions' do
-      expect(Version.versions.join("\n")).to eq("v7-4-083\nv7-4-103")
+    context 'vims dirctory exists' do
+      it 'echo installed vim versions' do
+        expect(Version.versions.join("\n")).to eq("v7-4-083\nv7-4-103")
+      end
+    end
+    context 'vims dirctory is not found' do
+      before do
+        FileUtils.rm_rf(get_vims_dir)
+      end
+      it 'echo nothing' do
+        expect(Version.versions).to eq []
+      end
     end
   end
 end
